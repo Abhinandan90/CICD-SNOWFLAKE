bf7957dd13ebde4403fae0949c1ac5b13a52bd59
Abhinandan-jn <abhinandan.sanoor@sunrun.com>
2025-04-20 23:08:28 +0530

    change privilage

--- modules/snowflake_resources/grants.tf
+++ modules/snowflake_resources/grants.tf
@@ -2,42 +2,13 @@ resource "snowflake_account_role" "tf_demo_reader" {
   name = "TF_DEMO_READER"
 }
 
-# Grant USAGE on the Database
+resource "snowflake_database_role" "tf_demo_reader" {
+  database = snowflake_database.tf_demo_database.name
+  name     = "TF_DEMO_READER"
+}
+
 resource "snowflake_grant_privileges_to_database_role" "grant_usage_on_db" {
+  database_role_name = snowflake_database_role.tf_demo_reader.fully_qualified_name
+  on_database        = snowflake_database_role.tf_demo_reader.database
   privileges         = ["USAGE"]
-  database_role_name = snowflake_account_role.tf_demo_reader.name
-  on_database        = snowflake_database.tf_demo_database.name
-}
-
-# Grant USAGE on the Schema
-resource "snowflake_grant_privileges_to_database_role" "grant_usage_on_schema" {
-  privileges         = ["USAGE"]
-  database_role_name = snowflake_account_role.tf_demo_reader.name
-  on_schema {
-    schema_name = "${snowflake_database.tf_demo_database.name}.${snowflake_schema.tf_demo_schema.name}"
-  }
-}
-
-# Grant SELECT on all future Tables
-resource "snowflake_grant_privileges_to_database_role" "grant_select_on_future_tables" {
-  privileges         = ["SELECT"]
-  database_role_name = snowflake_account_role.tf_demo_reader.name
-  on_schema_object {
-    future {
-      object_type_plural = "TABLES"
-      in_schema          = "${snowflake_database.tf_demo_database.name}.${snowflake_schema.tf_demo_schema.name}"
-    }
-  }
-}
-
-# Grant SELECT on all future Views
-resource "snowflake_grant_privileges_to_database_role" "grant_select_on_future_views" {
-  privileges         = ["SELECT"]
-  database_role_name = snowflake_account_role.tf_demo_reader.name
-  on_schema_object {
-    future {
-      object_type_plural = "VIEWS"
-      in_schema          = "${snowflake_database.tf_demo_database.name}.${snowflake_schema.tf_demo_schema.name}"
-    }
-  }
 }
