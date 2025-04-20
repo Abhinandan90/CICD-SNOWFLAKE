4f96f4f5a95b2d71a139fccda779e77b64f6f64f
Abhinandan-jn <abhinandan.sanoor@sunrun.com>
2025-04-20 22:34:32 +0530

    change role

--- modules/snowflake_resources/grants.tf
+++ modules/snowflake_resources/grants.tf
@@ -1,19 +1,18 @@
-# Create Role
-resource "snowflake_role" "tf_demo_reader" {
+resource "snowflake_account_role" "tf_demo_reader" {
   name = "TF_DEMO_READER"
 }
 
 # Grant USAGE on the Database
 resource "snowflake_grant_privileges_to_database_role" "grant_usage_on_db" {
   privileges         = ["USAGE"]
-  database_role_name = snowflake_role.tf_demo_reader.name
+  database_role_name = snowflake_account_role.tf_demo_reader.name
   on_database        = snowflake_database.tf_demo_database.name
 }
 
 # Grant USAGE on the Schema
 resource "snowflake_grant_privileges_to_database_role" "grant_usage_on_schema" {
   privileges         = ["USAGE"]
-  database_role_name = snowflake_role.tf_demo_reader.name
+  database_role_name = snowflake_account_role.tf_demo_reader.name
   on_schema {
     schema_name = "${snowflake_database.tf_demo_database.name}.${snowflake_schema.tf_demo_schema.name}"
   }
@@ -22,7 +21,7 @@ resource "snowflake_grant_privileges_to_database_role" "grant_usage_on_schema" {
 # Grant SELECT on all future Tables
 resource "snowflake_grant_privileges_to_database_role" "grant_select_on_future_tables" {
   privileges         = ["SELECT"]
-  database_role_name = snowflake_role.tf_demo_reader.name
+  database_role_name = snowflake_account_role.tf_demo_reader.name
   on_schema_object {
     future {
       object_type_plural = "TABLES"
@@ -34,7 +33,7 @@ resource "snowflake_grant_privileges_to_database_role" "grant_select_on_future_t
 # Grant SELECT on all future Views
 resource "snowflake_grant_privileges_to_database_role" "grant_select_on_future_views" {
   privileges         = ["SELECT"]
-  database_role_name = snowflake_role.tf_demo_reader.name
+  database_role_name = snowflake_account_role.tf_demo_reader.name
   on_schema_object {
     future {
       object_type_plural = "VIEWS"
